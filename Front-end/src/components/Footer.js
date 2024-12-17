84611a85bc6c3113ca742384eaffdec43e240ab8
Rxunak <raunakuk2016@gmail.com>
2024-12-17 15:47:47 +0000

    Customised Footer with required changes

--- Front-end/src/components/Footer.js
+++ Front-end/src/components/Footer.js
@@ -1,12 +1,23 @@
 import "../styles/Footer.css";
+import { MdLocalPolice } from "react-icons/md";
+import { RiInstagramFill } from "react-icons/ri";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+
+
+
 
 const Footer = () => {
   return (
     <div className="mainfooter">
       <div className="footer">
-        <div>police icons</div>
-        <div>Coyright</div>
-        <div>Social media</div>
+        <div className="footerone"><MdLocalPolice/></div>
+        <div className="footertwo">&copy; 2024 ThreatDetect. All rights reserved.</div>
+        <div className="footerthree">
+          <li><a target="_blank" rel="noreferrer" href="https://www.instagram.com/" ><RiInstagramFill/></a></li>
+          <li><FaFacebook/></li>
+          <li><FaTwitter/></li>
+        </div>
       </div>
     </div>
   );
