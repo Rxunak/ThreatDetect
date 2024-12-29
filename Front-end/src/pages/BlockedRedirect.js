25bb2321dd3e3505f94e16383e5dd32a182d0a61
Rxunak <raunakuk2016@gmail.com>
2024-12-29 16:15:25 +0000

    Created a block page

--- Front-end/src/pages/BlockedRedirect.js
+++ Front-end/src/pages/BlockedRedirect.js
@@ -1,14 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import image from "../assets/image copy.png";
+import "../styles/BlockedPage.css";
+import { MdContactSupport } from "react-icons/md";
+import { IoLogOutSharp } from "react-icons/io5";
 
 const BlockedRedirect = () => {
-  console.log("Loaded");
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem("auth");
+    
+        navigate("/login");
+      };
   return (
-    <div>
-      <h1>You have been blocked</h1>
-      <Link to="/login">
-        <button>Login</button>
-      </Link>
+    <div className="mainBlockPage">
+      <div className="secondMainPage">
+        <div className="firstBlock">
+          <div className="blockPageHeading">
+            <h1>Access Restricted</h1>
+          </div>
+          <div className="blockPageImage">
+            <img className="blockPageImage1" src={image} alt="" />
+          </div>
+        </div>
+
+        <div className="secondBlock">
+          <div className="blockPageText">
+            <p>
+              Your account has been temporarily blocked due to <br />
+              suspicious activity. If you beleive this is a mistake, <br />
+              please contact support.
+            </p>
+          </div>
+
+          <div className="blockPageButton">
+            <button className="button1">
+              <a className="button2css" href="mailto:raunakuk2016@gmail.com"><MdContactSupport /></a>
+            </button>
+
+            <button className="button2" onClick={handleLogout}>
+              <IoLogOutSharp />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
