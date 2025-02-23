8178880bc24e29b63bab2967bea74ad33dbd759a
Rxunak <raunakuk2016@gmail.com>
2025-02-23 17:08:02 +0000

    Removed unrequired comments

--- Front-end/src/components/ProtectedRoute.js
+++ Front-end/src/components/ProtectedRoute.js
@@ -2,23 +2,19 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ element, requiredRole }) => {
-  const auth = JSON.parse(localStorage.getItem("auth")); // Retrieve auth from localStorage
+  const auth = JSON.parse(localStorage.getItem("auth"));
 
   if (!auth?.isAuthenticated) {
-    // Redirect to login if not authenticated
     return <Navigate to="/login" replace />;
   }
 
-  if (auth?.isBlocked){
-    return <Navigate to="/block" replace/>;
+  if (auth?.isBlocked) {
+    return <Navigate to="/block" replace />;
   }
 
   if (requiredRole && auth.role !== requiredRole) {
-    // Redirect to main page if role does not match requiredRole
     return <Navigate to="/" replace />;
   }
-
-
 
   return element;
 };
